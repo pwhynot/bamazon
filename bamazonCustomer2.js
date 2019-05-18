11e0c9acf3d9facc88e6b10c3f7669c6a4afd422
unknown <pwhynotfilms@gmail.com>
2019-05-17 21:49:51 -0500

    working prompts

--- bamazonCustomer2.js
+++ bamazonCustomer2.js
@@ -17,7 +17,7 @@ connection.connect(function(err){
 const findProducts = function() {
     const query = "SELECT * FROM products";
     connection.query(query, function(err, res) {
-        console.log('\n');
+        console.log("\n");
         for (var i = 0; i < res.length; i++) {
             console.log("Item ID:  " + res[i].item_id + " Product Name: " + res[i].product_name + 
             " Department: " + res[i].department_name + " Price: " + res[i].price + " Quanity: " 
@@ -28,5 +28,57 @@ const findProducts = function() {
 };
 
 const selectProduct = function() {
-    
+    inquirer.prompt([{
+        name: "id",
+        type: "input",
+        message: "Select Item ID of Product you would like to buy?",
+        validate: function(value) {
+            if (isNaN(value) == false) {
+                return true;
+
+            } else {
+                console.log("\nNot a valid ID number.\n");
+                return false;
+            } 
+        } 
+
+    	}, {
+
+        name: "total",
+        type: "input",
+        message: "How many would you like to buy?",
+        validate: function(value) { 
+            if (isNaN(value) == false) {                
+                return true;
+            } else {        
+                console.log("\nPlease enter total\n");
+                return false;
+            } 
+        } 
+        
+    	}]).then(function(answer) {         
+            console.log(answer);
+            IntItem = parseInt(answer.total);
+            console.log(IntItem);
+            connection.query("SELECT * FROM products WHERE ?", [{item_id: answer.id}], function(err, data) { 
+                if (err) throw err;
+                if (data[0].stock_quantity < IntItem) {
+                console.log("Sorry, the quanity selected is currently not availible, please select a lesser quanity");
+                searchDatabase();
+                }   
+                else {
+                    const newQuantity = data[0].stock_quantity - IntItem;
+                    const totalPrice = data[0].price * IntItem;
+                    connection.query("UPDATE products SET stock_quantity = ? WHERE item_id = ?", [newQuantity, answer.id], 
+                    function(err, results) {
+                        if (err) throw err;
+                        else {
+                            console.log("Congrats on your purchase! Your total cost is $"+ totalPrice);
+                            
+                        }
+                    });  
+                }
+            });
+        });
 }
+
